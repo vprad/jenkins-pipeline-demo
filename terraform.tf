917a082052bcc70b2947780588ad506b945a0a7b
vprad <100353910+vprad@users.noreply.github.com>
2023-07-27 19:25:43 +0530

    Update terraform.tf

--- terraform.tf
+++ terraform.tf
@@ -2,10 +2,11 @@ provider "aws" {
   region = var.region
 }
 
-resource "aws_instance" "example" {
+resource "aws_instance" "ex" {
   ami           = "ami-05548f9cecf47b442"
   instance_type = "t2.micro"
   tags = {
     Name = "Example EC2 Instance"
+    Environment = "Production"
   }
 }
