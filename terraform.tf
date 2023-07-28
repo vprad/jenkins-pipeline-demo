e8de41d7d4d453a471e580bd02ebc0765f867734
vprad <100353910+vprad@users.noreply.github.com>
2023-07-28 14:00:45 +0530

    Update terraform.tf

--- terraform.tf
+++ terraform.tf
@@ -6,7 +6,7 @@ resource "aws_instance" "ex" {
   ami           = "ami-05548f9cecf47b442"
   instance_type = "t2.micro"
   tags = {
-    Name = "jenkins-pipeline-demo"
+    Name = "terraform-ec2-instance"
     Environment = "Production"
   }
 }
