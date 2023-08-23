5cc2ffaa7eee6fdb9fdf30e73f9e4ccdaffdc9c6
vprad <100353910+vprad@users.noreply.github.com>
2023-08-23 21:10:59 +0530

    Update and rename terraform.tf to main.tf

---
+++ main.tf
@@ -0,0 +1,39 @@
+provider "aws" {
+  region = var.region  # Change to your desired AWS region
+}
+
+resource "aws_eks_cluster" "my_cluster" {
+  name     = "my-eks-cluster"
+  role_arn = aws_iam_role.eks_cluster.arn
+
+  vpc_config {
+    vpc_id = "vpc-073a176c9982bb6b3"
+    subnet_ids = ["subnet-09e5086cb5732ef92", "subnet-0c560d4762e67700y", "subnet-012bf135fc92de1b3", "subnet-09ed9c8d387893fe4", "subnet-07be01196f968b01d"]  # Change these to your desired subnet IDs
+  }
+}
+
+resource "aws_iam_role" "eks_cluster" {
+  name = "eks-cluster-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [{
+      Action = "sts:AssumeRole",
+      Effect = "Allow",
+      Principal = {
+        Service = "eks.amazonaws.com"
+      }
+    }]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "eks_cluster_policy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+  role       = aws_iam_role.eks_cluster.name
+}
+
+resource "aws_iam_role_policy_attachment" "eks_service_policy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSServicePolicy"
+  role       = aws_iam_role.eks_cluster.name
+}
+
