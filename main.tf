480e091443eca6ca56470d77170bae583d4c3f8b
vprad <100353910+vprad@users.noreply.github.com>
2023-08-25 12:16:52 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,93 +1,64 @@
+terraform {
+required_providers {
+aws = {
+source = "hashicorp/aws"
+version = "~> 5.0"
+}
+}
+}
+
 provider "aws" {
-  region = var.region  # Change to your desired AWS region
+region = var.region
 }
 
-resource "aws_eks_cluster" "my_cluster" {
-  name     = "my-eks-cluster"
-  role_arn = aws_iam_role.eks_cluster.arn
+resource "aws_instance" "jenkins-server" {
+ami = "ami-0aa7d40eeae50c9a9"
+instance_type = "t2.micro"
 
-  vpc_config {
-    subnet_ids = ["subnet-09e5086cb5732ef92", "subnet-0c560d4762e677007", "subnet-012bf135fc92de1b3", "subnet-09ed9c8d387893fe4", "subnet-07be01196f968b01d"]
-  }
+tags = {
+Name = "Jenkins-prod"
 }
 
-resource "aws_iam_role" "eks_cluster" {
-  name = "eks-cluster-role"
-
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17",
-    Statement = [{
-      Action = "sts:AssumeRole",
-      Effect = "Allow",
-      Principal = {
-        Service = "eks.amazonaws.com"
-      }
-    }]
-  })
+user_data = <<EOF
+#!/bin/bash
+yum update -y
+yum update –y
+wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo
+rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io.key
+yum upgrade -y
+amazon-linux-extras install java-openjdk11 -y
+yum install jenkins -y
+systemctl enable jenkins
+systemctl start jenkins
+EOF
 }
+resource "aws_security_group" "jenkinssg" {
+name = "jenkinssg"
+description = "Allow SSH and HTTP Traffic"
 
-resource "aws_iam_role_policy_attachment" "eks_cluster_policy" {
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
-  role       = aws_iam_role.eks_cluster.name
+ingress {
+from_port = 22
+to_port = 22
+protocol = "tcp"
+cidr_blocks = ["0.0.0.0/0"]
 }
-
-resource "aws_iam_role_policy_attachment" "eks_service_policy" {
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSServicePolicy"
-  role       = aws_iam_role.eks_cluster.name
+ingress {
+from_port   = 80
+to_port     = 80
+protocol    = "tcp"
+cidr_blocks = ["0.0.0.0/0"]
 }
-
-resource "aws_eks_node_group" "my_node_group" {
-  cluster_name    = aws_eks_cluster.my_cluster.name
-  node_group_name = "my-node-group"
-
-  scaling_config {
-    desired_size = 2
-    max_size     = 2
-    min_size     = 1
+ingress {
+from_port   = 443
+to_port     = 443
+protocol    = "tcp"
+cidr_blocks = ["0.0.0.0/0"]
   }
-
-  remote_access {
-    ec2_ssh_key = "my-ssh-key"
-  }
-
-  node_role_arn = aws_iam_role.eks_cluster.arn
-  subnet_ids    = ["subnet-09e5086cb5732ef92", "subnet-0c560d4762e677007", "subnet-012bf135fc92de1b3", "subnet-09ed9c8d387893fe4", "subnet-07be01196f968b01d"]  // Choose the appropriate subnets
-
-  instance_types = ["t2.medium"]
-
-  tags = {
-    Terraform = "true"
-  }
+ingress {
+from_port = 8080
+to_port = 8080
+protocol = "tcp"
+cidr_blocks = ["0.0.0.0/0"]
 }
-
-resource "aws_security_group" "my_security_group" {
-  name_prefix = "my-security-group-"
-  
-  vpc_id = aws_eks_cluster.my_cluster.vpc_config[0].vpc_id
-
-  ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    from_port   = 443
-    to_port     = 443
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    from_port   = 8080  // Change to your desired custom TCP port
-    to_port     = 8080  // Change to your desired custom TCP port
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  // Add more ingress and egress rules if needed
-}
-
 
 
